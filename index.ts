eeb17741c71892a9e4678592cb0667f848b2645d
andremaurell <andremaurell3@gmail.com>
2024-03-10 15:01:40 -0300

    🔥 Remove: remove db directory to user create your own!

--- index.ts
+++ index.ts
@@ -14,7 +14,7 @@ app.use(router);
 
 const pool = new Pool({
     user: process.env.PGUSER,
-    host: process.env.PGHOST,
+    host: 'db',
     database: process.env.PGDATABASE,
     password: process.env.PGPASSWORD,
     port: parseInt(process.env.PGPORT || '5432', 10),
